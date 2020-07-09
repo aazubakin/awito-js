bae15f29a608a676b68de91984eb714e89941e6d
Aleksandr Zubakin <eldoson@yandex.ru>
2020-07-09 10:57:47 +0500

    Закрытие модальных окон через отдельные функции

--- scripts/script.js
+++ scripts/script.js
@@ -4,40 +4,48 @@ const modalAdd = document.querySelector('.modal__add'),
    addAd = document.querySelector('.add__ad'),
    modalBtnSubmit = document.querySelector('.modal__btn-submit'),
    modalSubmit = document.querySelector('.modal__submit'),
-   catalogModal = document.querySelector('.catalog'),
+   catalog = document.querySelector('.catalog'),
    modalItem = document.querySelector('.modal__item');
 
-addAd.addEventListener('click', () => {
-   modalAdd.classList.remove('hide'); //открытие модального окна 
-   modalBtnSubmit.disabled = true; // отключение кнопки при пустых полях
-});
+const elementsModalSubmit = modalSubmit.elements;
+console.log([...elementsModalSubmit].filter((elem) => {
+   return elem.tagName !== 'BUTTON';
+}));
 
-modalAdd.addEventListener('click', event => {
+const closeModal = function (event) {
    const target = event.target;
 
-   if (target.classList.contains('modal__close') || target === modalAdd) {
-      modalAdd.classList.add('hide'); //закрытие при клике вне модального окна
-      modalSubmit.reset(); //очищение полей формы при закрытии
+   if (target.closest('.modal__close') || target === this) {
+      this.classList.add('hide'); //закрытие при клике вне модального окна
+      if (this === modalAdd) {
+         modalSubmit.reset(); //очищение полей формы при закрытии
+      }
    }
-});
-
-catalogModal.addEventListener('click', () => {
-   modalItem.classList.remove('hide'); // открытие при клике на карточку
-});
-
-modalItem.addEventListener('click', event => {
-   const target = event.target;
-
-   if (target.classList.contains('modal__close') || target === modalItem) {
-      modalItem.classList.add('hide'); //закрытие при клике вне модального окна
-      modalSubmit.reset(); //очищение полей формы при закрытии
-   }
-});
-
-document.addEventListener('keydown', event => {
+};
+const closeModalEsc = (event) => {
    const keyName = event.key;
    if (keyName === 'Escape') {
       modalItem.classList.add('hide');
       modalAdd.classList.add('hide');
-   }
+      modalSubmit.reset(); //очищение полей формы при закрытии
+      document.removeEventListener('keydown', closeModalEsc); //Закрытие по клавише Escape
+   };
+};
+
+addAd.addEventListener('click', () => {
+   modalAdd.classList.remove('hide'); //открытие модального окна 
+   modalBtnSubmit.disabled = true; // отключение кнопки при пустых полях
+   document.addEventListener('keydown', closeModalEsc); //Закрытие по клавише Escape
 });
+
+catalog.addEventListener('click', event => {
+   const target = event.target;
+
+   if (target.closest('.card')) { //  определяем делигирование
+      modalItem.classList.remove('hide'); // открытие при клике на карточку
+      document.addEventListener('keydown', closeModalEsc); //Закрытие по клавише Escape
+   };
+});
+
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);

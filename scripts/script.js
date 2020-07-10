743a279dcfe2e9ffee2b96e861e462ea0d717c31
Aleksandr Zubakin <eldoson@yandex.ru>
2020-07-10 13:41:35 +0500

    Сохранение объектов в localStorge

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const dataBase = [];
+const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
 
 const modalAdd = document.querySelector('.modal__add'),
    addAd = document.querySelector('.add__ad'),
@@ -11,37 +11,48 @@ const modalAdd = document.querySelector('.modal__add'),
    modalBtnWarning = document.querySelector('.modal__btn-warning');
 
 const elementsModalSubmit = [...modalSubmit.elements]
-   .filter(elem => elem.tagName !== 'BUTTON' && elem.type !== 'submit'); // получаем все элементы формы кроме submit
+   .filter(elem => elem.tagName !== 'BUTTON'); // получаем все элементы формы кроме submit
 
-const closeModal = function (event) {
+const localDB = () => localStorage.setItem('awito', JSON.stringify(dataBase));
+
+const checkForm = () => {
+   const validForm = elementsModalSubmit.every(elem => elem.value);
+   //перебираем все элементы которые есть в форме, если они заполнены every вернет true
+   modalBtnSubmit.disabled = !validForm;
+   modalBtnWarning.style.display = validForm ? 'none' : ''; // убираем надпись
+}; //проверка формы на валидацию
+
+
+const closeModal = event => {
    const target = event.target;
-   const key = event.code;
-   if (target.closest('.modal__close') || target === this || key === 'Escape') {
+
+   if (target.closest('.modal__close') ||
+      target.classList.contains('modal') ||
+      event.code === 'Escape') {
       modalItem.classList.add('hide');
       modalAdd.classList.add('hide');//закрытие при клике вне модального окна
       if (modalAdd) {
          modalSubmit.reset(); //очищение полей формы при закрытии
-         document.removeEventListener('keydown', closeModalEsc); //Закрытие по клавише Escape
+         document.removeEventListener('keydown', closeModal); //После закрытия отключить прослушку
+         checkForm();
       }
    }
 };
 
-modalSubmit.addEventListener('input', () => {
-   const validForm = elementsModalSubmit.every(elem => elem.vlaue);
-   console.log(validForm);
-   modalBtnSubmit.disabled = !validForm;
-   modalBtnWarning.getElementsByClassName.display = validForm ? 'none' : '';
-}); // после заполнения полей активируется кнопака отправить и исчезает предупреждающая надпись
+modalSubmit.addEventListener('input', checkForm); // после заполнения полей активируется кнопака отправить и исчезает предупреждающая надпись
+
+
 
 modalSubmit.addEventListener('submit', event => {
    event.preventDefault(); // после отправки формы страница не перезагружается
    const itemObj = {};
    for (const elem of elementsModalSubmit) {
       itemObj[elem.name] = elem.value;
-   }
-
-   dataBase.push(itemObj);
+   }; // перебираем все элементы модального окна
+   dataBase.push(itemObj);// в ранее созданный массив добавляем наш объект
    modalSubmit.reset();
+   closeModal({ target: modalAdd });
+   localDB();
 }); // добавляем отправку данных формы в массив
 
 addAd.addEventListener('click', () => {
